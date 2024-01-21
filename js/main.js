821300d26001f6fc42b7cdfb459c3d2dcb6bab6c
Yaroslav Semenov <87042339+YaroslavSemenow@users.noreply.github.com>
2024-01-21 23:55:25 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -14,3 +14,48 @@
     headerEl.style.backgroundColor = "transparent";
   });
 })();
+
+// =================== приховуємо кастомний плейсхолдер, коли інпут не пустий ==========================
+
+(function toggleCustomPlaceholder() {
+  const inputWrapArr = document.querySelectorAll(".form-field__wrapper");
+  const hideCustomPlaceholderClass = "form-field__custom-placeholder--hidden";
+
+  if (!inputWrapArr?.length) return;
+
+  inputWrapArr.forEach((inputWrapEl) => {
+    const input = inputWrapEl.querySelector(".form-field");
+    const customPlaceholder = inputWrapEl.querySelector(
+      ".form-field__custom-placeholder"
+    );
+
+    input.addEventListener("input", (e) => {
+      if (e.target.value.trim() === "") {
+        customPlaceholder.classList.remove(hideCustomPlaceholderClass);
+      } else {
+        customPlaceholder.classList.add(hideCustomPlaceholderClass);
+      }
+    });
+  });
+})();
+
+// =================== swiper з дотами ==========================
+
+(function instSwiperWithDots() {
+  var swiper = new Swiper(".swiper-with-dots-container", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-with-dots-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return (
+          `<span class="${className}">
+            <span class="${className}__inner"></span>
+          </span>`
+        );
+      },
+    },
+   
+  });
+})();
