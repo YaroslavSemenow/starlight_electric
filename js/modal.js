a6b46de8859cf7846a8d8ae0dde2f6a065c339da
Yaroslav Semenov <87042339+YaroslavSemenow@users.noreply.github.com>
2024-01-28 02:11:08 +0200

    added modals

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,33 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
+const toggleModal = (openModalBtnArr, modal, closeModalBtn) => {
+  if (!openModalBtnArr?.length || !closeModalBtn || !modal) {
+    return;
+  }
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+  const toggleClass = () => modal.classList.toggle("is-hidden");
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-// })();
+  openModalBtnArr.forEach((btn) => {
+    btn.addEventListener("click", toggleClass);
+  });
+
+  closeModalBtn.addEventListener("click", toggleClass);
+};
+
+// модальне вікно з продукцією
+(() => {
+  const openModalBtnArr = document.querySelectorAll("[data-product-modal-open]");
+
+  const modal = document.querySelector("[data-product-modal]");
+  const closeModalBtn = document.querySelector("[data-product-modal-close]");
+
+  toggleModal(openModalBtnArr, modal, closeModalBtn);
+})();
+
+// модальне вікно замовлення
+(() => {
+  const openModalBtnArr = document.querySelectorAll("[data-order-modal-open]");
+
+  const modal = document.querySelector("[data-order-modal]");
+  const closeModalBtn = document.querySelector("[data-order-modal-close]");
+
+  toggleModal(openModalBtnArr, modal, closeModalBtn);
+})();
